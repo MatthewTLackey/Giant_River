feb506f7f3812b7e7836d815ee6f614db2544632
MatthewTLackey <lackey.matt@gmail.com>
2013-11-13 15:23:14 -0500

    Adds authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -10,4 +12,12 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_user  
+
+  def ensure_logged_in
+    unless current_user
+      flash[:alert] = "Please log in"
+      redirect_to new_session_path
+    end
+  end
+
 end
